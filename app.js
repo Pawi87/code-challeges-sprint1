379a6ddb737ed281ba56f750e09d5ba0a494d5d4
pawi87 <arellanopaulina12@gmail.com>
2017-10-25 12:42:40 +0000

    listo

---
+++ app.js
@@ -0,0 +1,12 @@
+function findshortestofthreewords (word1, word2, word3){
+  var lng1 = word1.lenght;
+  var lng1 = word2.lenght;
+  var lng1 = word3.lenght;
+
+  if (lng1 <= lng2 && lng1 <= lng3){
+    return word1;
+
+  }
+  findshortestofthreewords ("mueblería", "pizaron","descanso");
+
+}
